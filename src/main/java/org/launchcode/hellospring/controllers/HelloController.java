2d5a49aac7438f92459624f082275ab4f4eb5921
LRyan <lwryan6@gmail.com>
2022-10-02 23:39:28 -0500

    Completed views-dynamic video practice.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,47 +2,45 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
-    // Handles request at path </hello>
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello( ) {
-//        return "Hello, Spring!";
-//    }
-
-    // Handles request at path </goodbye>     Turned off b/c not needed for this practice video.
-//    @GetMapping("goodbye")
-//    @ResponseBody
-//    public String goodbye( ) {
-//        return "Goodbye, Spring!";
-//    }
-
-    // Handles request of the form /hello?name=LaunchCode
-    // Must comment off lines 13-17 above to work here b/c cannot have 2 different controllers at same path ("hello")
-
-//    @GetMapping("hello")     // USE this for GET request methods if don't need both (things default to GET if not stated)
-
+    // Handles request at /hello?name=LaunchCode
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST}) // Use this for different request types
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {  // Add Model object to pass data to template.
+        String theGreeting = "Hello, " + name + "!";  // You can cut this line and add string value directly in () below.
+        model.addAttribute("greeting", theGreeting);  // You can replace theGreeting w/ 'Hello, ' + name + '!' from line above.
+        return "hello";     // Refactored this to return the name of the template "hello" from hello.html
     }
 
-    // Handles request of the form /hello/LaunchCode
+    // Handles request at /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!"); // DRY version w/ no theGreeting variable.
+        return "hello";
     }
 
     // Handles request at /form  (or rather at localhost:8080/form)
     @GetMapping("form")  // ("form") requires you put <localhost:8080/form> If no ("form"), path defaults to / root <localhost:8080>
-//    @ResponseBody     // Do not need this when you use templates! It will ASSUME you want
+//    @ResponseBody  // Use to return literal String text. Otherwise, not needed w/ templates b/c string template name assumed.
     public String helloForm() {
         return "form";  //  the name of the form.html template file to tell it where to go to find the form html info.
+    }
+
+    // Handles request at /hello-names
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> theNames = new ArrayList<>();
+        theNames.add("LaunchCode");
+        theNames.add("Java");
+        theNames.add("JavaScript");
+        model.addAttribute("names", theNames);
+        return "hello-list";
     }
 }
